3e7d071954b44775e204a602bb857fe830a96d5f
Ahmed Ellashy <ahmad.ellashy@3pillars-eg.com>
2026-03-25 21:22:25 +0200

    feat: project structure + AppCoordinator entry point

---
+++ Pulse/Common/Coordinators/AppCoordinator.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+final class AppCoordinator: Coordinator {
+    var childCoordinators: [Coordinator] = []
+    private let window: UIWindow
+
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    func start() {
+        let nav = UINavigationController()
+        window.rootViewController = nav
+        window.makeKeyAndVisible()
+
+        // TODO: launch DashboardCoordinator here next
+    }
+}
